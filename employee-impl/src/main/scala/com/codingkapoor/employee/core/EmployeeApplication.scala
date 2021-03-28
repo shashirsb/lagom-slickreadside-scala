38060261b7bdabf5bae772f3108fa73e3b9dcc98
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2021-03-28 16:06:47 +0530

    reverting to cassandra

--- employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
@@ -7,7 +7,6 @@ import com.codingkapoor.employee.service.EmployeeServiceImpl
 import com.lightbend.lagom.scaladsl.persistence.cassandra.WriteSideCassandraPersistenceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext}
 import com.lightbend.lagom.scaladsl.persistence.slick.ReadSideSlickPersistenceComponents
-import com.lightbend.lagom.scaladsl.persistence.jdbc.JdbcPersistenceComponents
 import com.softwaremill.macwire._
 import play.api.db.HikariCPComponents
 import play.api.libs.ws.ahc.AhcWSComponents
@@ -15,7 +14,7 @@ import play.api.libs.ws.ahc.AhcWSComponents
 abstract class EmployeeApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
     with ReadSideSlickPersistenceComponents
-    with JdbcPersistenceComponents
+    with WriteSideCassandraPersistenceComponents
     with HikariCPComponents
     with AhcWSComponents {
 
