8b904468b2b12f563b6291abf9163c81fa25f0a5
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2021-03-28 15:52:06 +0530

    adding write side code

--- employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
@@ -7,6 +7,7 @@ import com.codingkapoor.employee.service.EmployeeServiceImpl
 import com.lightbend.lagom.scaladsl.persistence.cassandra.WriteSideCassandraPersistenceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext}
 import com.lightbend.lagom.scaladsl.persistence.slick.ReadSideSlickPersistenceComponents
+import com.lightbend.lagom.scaladsl.persistence.jdbc.JdbcPersistenceComponents
 import com.softwaremill.macwire._
 import play.api.db.HikariCPComponents
 import play.api.libs.ws.ahc.AhcWSComponents
@@ -14,7 +15,7 @@ import play.api.libs.ws.ahc.AhcWSComponents
 abstract class EmployeeApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
     with ReadSideSlickPersistenceComponents
-    with WriteSideCassandraPersistenceComponents
+    with JdbcPersistenceComponents
     with HikariCPComponents
     with AhcWSComponents {
 
